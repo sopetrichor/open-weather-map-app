d6d738ade656da3c22f05bf9c9ef3e47f43645fa
sopetrichor <stcwer@gmail.com>
2019-11-24 23:25:11 +0800

    add mode and units parameters

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 const API_KEY = process.env.API_KEY || 'unkonwn_api_key';
 const API_QS_WITH_APPID = `&APPID=${API_KEY}`;
 const API_VERSION = '2.5';
-const API_ROOT = `https://api.openweathermap.org/data/${API_VERSION}/`;
+const API_ROOT = `https://api.openweathermap.org/data/${API_VERSION}`;
+const API_MODE = '&mode=json';
+const API_UNITS = '&units=metric';
 
 export const callApi = (endpoint: string) => {
   const fullUrl =
@@ -25,4 +27,4 @@ export const callApi = (endpoint: string) => {
   );
 };
 
-export const fetchByCityName = (cityName: string) => callApi(`find?q=${cityName}`);
+export const fetchByCityName = (cityName: string) => callApi(`/forecast/daily?q=${cityName}` + API_MODE + API_UNITS);
