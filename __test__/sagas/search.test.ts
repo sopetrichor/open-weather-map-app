7ce80f0328f75aa97cca6f9f655d704952b9ab00
sopetrichor <stcwer@gmail.com>
2019-12-07 20:13:05 +0800

    exact order with redux-saga-test-plan

--- __test__/sagas/search.test.ts
+++ __test__/sagas/search.test.ts
@@ -1,4 +1,5 @@
 import { take, takeLatest, put, call, fork, select } from 'redux-saga/effects';
+import { testSaga, expectSaga } from 'redux-saga-test-plan';
 
 import { AT_SEARCH, AC_SEARCH } from '../../src/actions';
 import { watcher, worker, fetchByCityName } from '../../src/sagas/search';
@@ -12,11 +13,14 @@ describe('test search saga watcher', () => {
     gWatcher = watcher();
   });
 
-  test(`it should wait for a search request action`, () => {
+  test('it should wait for a search request action', () => {
     expect(gWatcher.next().value).toEqual(take(AT_SEARCH.REQUEST));
   });
   test('it should fork a search task with payload', () => {
     expect(gWatcher.next(AC_SEARCH.request(payload)).value).toEqual(fork(worker, payload));
+  });
+  test('it should be done', () => {
+    expect(gWatcher.next().done).toBe(true);
   });
 });
 
@@ -31,5 +35,34 @@ describe('test search saga worker', () => {
 
   test('it should invoke fetchByCityName with payload', () => {
     expect(gWorker.next().value).toEqual(call(fetchByCityName, payload));
+  });
+  test('it should be done', () => {
+    expect(gWorker.next().done).toBe(true);
+  });
+});
+
+describe('test search saga watcher [redux-saga-test-plan]', () => {
+  const payload = { cityName: 'Taipei' };
+
+  test('exact order with redux-saga-test-plan', () => {
+    testSaga(watcher)
+      .next()
+      .take(AT_SEARCH.REQUEST)
+      .next(AC_SEARCH.request(payload))
+      .fork(worker, payload)
+      .next()
+      .isDone();
+  });
+});
+
+describe('test search saga worker [redux-saga-test-plan]', () => {
+  const payload = { cityName: 'Taipei' };
+
+  test('exact order with redux-saga-test-plan', () => {
+    testSaga(worker, payload)
+      .next()
+      .call(fetchByCityName, payload)
+      .next()
+      .isDone();
   });
 });
