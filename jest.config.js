d12c0b1f78c3e68683f2a220550172fc1217da5b
sopetrichor <stcwer@gmail.com>
2020-01-01 19:24:10 +0800

    add jest moduleNameMapper option to handle webpack alias

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  roots: ['__test__'],
+  roots: ['<rootDir>/__tests__/'],
+  moduleNameMapper: {
+    '^@actions/(.*)$': '<rootDir>/src/actions/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@parsers/(.*)$': '<rootDir>/src/parsers/$1',
+    '^@prop-types/(.*)$': '<rootDir>/src/prop-types/$1',
+    '^@sagas/(.*)$': '<rootDir>/src/sagas/$1',
+    '^@reducers/(.*)$': '<rootDir>/src/reducers/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+  },
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
